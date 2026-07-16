58b4ef751f7a2d178b3015c569ba3239e12116f7
Chengshun Shang <chengshun.shang1996@gmail.com>
2026-07-16 15:57:40 +0200

    minor

--- src/fastslippy/solver/fault_state.py
+++ src/fastslippy/solver/fault_state.py
@@ -189,7 +189,8 @@ class FaultState:
                     )
         
         if p.case_type == "california":
-            self.V[(p.W_f // (p.ysize / (p.Ny - 1)) + 1) : p.Ny] =  p.loading.V_L
+            start_idx = int(p.W_f // (p.ysize / (p.Ny - 1)) + 1)
+            self.V[start_idx : p.Ny] = p.loading.V_L
 
         self.V = np.maximum(self.V, 1e-40)
 
